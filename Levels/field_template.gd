6473c78f55ad6669a3bfa663294af76473620eab
Doominator-726 <nate.r.laing@gmail.com>
2026-09-19 17:45:55 -0400

    Weapon Resource Fixed

--- Levels/field_template.gd
+++ Levels/field_template.gd
@@ -3,5 +3,5 @@ extends Node2D
 @export var spawn_positon: Vector2 = Vector2.ZERO
 @export var bonus_points: int = 40
 
-func _on_bad_timer_timeout() -> void:
+func _process(delta: float) -> void:
 	Globals.field_time_left = $BadTimer.time_left
